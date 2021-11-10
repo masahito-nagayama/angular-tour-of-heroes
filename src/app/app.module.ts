d98717943722cb4f5cde4223371f0b9e4dd48186
masahito-nagayama <me18sf67m@gmail.com>
2021-11-11 08:14:38 +0900

    add hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 @NgModule({
